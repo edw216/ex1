260e71c903ad1c825fe856a9ecc096bfca8b4bcf
dongwoo <ehddn216@gmail.com>
2020-10-31 03:59:44 +0900

    commit

--- ex2/src/main/java/com/study/server/ex2/HomeController.java
+++ ex2/src/main/java/com/study/server/ex2/HomeController.java
@@ -1,13 +1,50 @@
 package com.study.server.ex2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+    int com_result = (int)(Math.random()*(100-1)+1);
+    int count =6;
     @RequestMapping(method = RequestMethod.GET, value = "/main")
     public String getMain() {
         return "main/index";
+    }
+
+
+    @GetMapping("/main/echo")
+    public ModelAndView getEcho(@RequestParam Integer User_result1){
+        ModelAndView result = new ModelAndView("main/echo");
+        String remainCount="";
+        String outcome ="";
+        if(count == 0){
+            remainCount="기회를 모두 사용했습니다. 탈락입니다.";
+            outcome = "정답은 "+com_result+"입니다.!";
+            com_result = (int)(Math.random()*(100-1)+1);
+            count = 6;
+
+        }
+        else if(com_result!=User_result1) {
+            if(com_result>User_result1){
+                outcome = "정답은 더 큰 숫자 입니다.";
+            }else if(com_result<User_result1){
+                outcome = "정답은 더 작은 숫자 입니다.";
+            }
+            count = count-1;
+            remainCount = count + "번의 기회가 남았습니다.";
+        }else{
+            remainCount = "정답입니다.!";
+            outcome = "정답:"+com_result;
+            com_result = (int)(Math.random()*(100-1)+1);
+            count =6;
+        }
+        result.getModel().put("remainCount",remainCount);
+        result.getModel().put("outcome",outcome);
+        return result;
     }
 }
--- src/main/java/com/study/server/ex1/controller/BoardController.java
+++ src/main/java/com/study/server/ex1/controller/BoardController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.soap.SOAPBinding;
+
 @Controller
 //@RequestMapping("/board")
 public class BoardController {
@@ -19,8 +21,32 @@ public class BoardController {
         ModelAndView result = new ModelAndView("board/echo");
         result.getModel().put("User_result1", User_result1);
         result.getModel().put("User_result2", User_result2);
+        Integer User = User_result1 + User_result2;
+        result.getModel().put("User",User);
+        Integer comDice1 = (int)(Math.random()*(6-1)+1);
+        Integer comDice2 = (int)(Math.random()*(6-1)+1);
 
+
+        Integer com_result = getComputer(comDice1,comDice2);
+        result.getModel().put("comDice1",comDice1);
+        result.getModel().put("comDice2",comDice2);
+        result.getModel().put("com_result",com_result);
+        String dice_result="";
+        if(com_result > User){
+            dice_result = "컴퓨터 승!";
+        }
+        else if(com_result < User){
+            dice_result = "유저 승!";
+        }else{
+            dice_result = "무승부 !";
+        }
+
+        result.getModel().put("dice_result",dice_result);
         return result;
+    }
+    public Integer getComputer(Integer com1,Integer com2){
+        return com1+com2;
+
     }
 
     /*@PostMapping("/board/echo")
